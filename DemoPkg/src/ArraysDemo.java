6ac0b1558ec2b23c528bc505094cf5b656ea04c0
abdkamal <abdkamal@capgemini.com>
2018-01-17 13:00:07 +0800

    Init commit

---
+++ DemoPkg/src/ArraysDemo.java
@@ -0,0 +1,35 @@
+
+//import java.lang.String;
+
+import static java.lang.System.out;
+
+class String{
+	
+}
+
+
+public class ArraysDemo{
+	
+	public static void main(java.lang.String[] args){
+		
+		//int values[] = new int[10];
+		
+		acceptValues(new int[]
+			{
+				10,20,30,40
+			}
+		);
+		
+		
+	}
+	
+	public static void acceptValues(int[] values){
+		
+		for(int value: values){
+			out.print(value + "\t");
+		}
+		
+	}
+	
+	
+}
