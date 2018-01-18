4a016e4f75381273668ba2ca8f7b13fc19c97b04
abdkamal <abdkamal@capgemini.com>
2018-01-18 11:02:58 +0800

    main.java change

--- LabWork/src/Main/Main.java
+++ LabWork/src/Main/Main.java
@@ -5,25 +5,27 @@ public class Main
 
     public static void main(String[] args)
     {
-	Employee a = new Employee(Employee.getEmployeeCount()+1, "abc", "def", 2000, 'a', "01/01/2017");
+	Employee a = new Employee(Employee.getEmployeeCount() + 1, "abc", "def", 2000, 'a', "01/01/2017");
 	Employee.printTotalEmpoyee();
-	Employee b = new Employee(Employee.getEmployeeCount()+1, "abc", "def", 2000, 'a', "01/01/2017");
+	Employee b = new Employee(Employee.getEmployeeCount() + 1, "abc", "def", 2000, 'a', "01/01/2017");
 	Employee.printTotalEmpoyee();
-	Employee c = new Employee(Employee.getEmployeeCount()+1, "abc", "def");
+	Employee c = new Employee(Employee.getEmployeeCount() + 1, "abc", "def");
 	Employee.printTotalEmpoyee();
-	Employee d = new Employee(Employee.getEmployeeCount()+1, "123", "456");
+	Employee d = new Employee(Employee.getEmployeeCount() + 1, "123", "456");
 	Employee.printTotalEmpoyee();
-	
-	Employee g = new ContractEmployee(Employee.getEmployeeCount()+1, "abc", "def",8.75,8,'a',"01/01/2017","Accenture");
+
+	Employee g = new ContractEmployee(Employee.getEmployeeCount() + 1, "abc", "def", 8.75, 8, 'a', "01/01/2017",
+		"Accenture");
 	ContractEmployee.printTotalConEmpoyee();
-	
-	Employee e = new PermanentEmployee(Employee.getEmployeeCount()+1, "abc", "def", 2000, 'a', "01/01/2017");
+
+	Employee e = new PermanentEmployee(Employee.getEmployeeCount() + 1, "abc", "def", 2000, 'a', "01/01/2017");
 	PermanentEmployee.printTotalPermEmpoyee();
-	
-	Employee f = new PermanentEmployee(Employee.getEmployeeCount()+1, "abc", "def", 2000, 'a', "01/01/2017");
+
+	Employee f = new PermanentEmployee(Employee.getEmployeeCount() + 1, "abc", "def", 2000, 'a', "01/01/2017");
 	PermanentEmployee.printTotalPermEmpoyee();
-		
-	Employee h = new ContractEmployee(Employee.getEmployeeCount()+1, "abc", "def", 8.75,8, 'a', "01/01/2017", "Capgemini");
+
+	Employee h = new ContractEmployee(Employee.getEmployeeCount() + 1, "abc", "def", 8.75, 8, 'a', "01/01/2017",
+		"Capgemini");
 	ContractEmployee.printTotalConEmpoyee();
     }
 
