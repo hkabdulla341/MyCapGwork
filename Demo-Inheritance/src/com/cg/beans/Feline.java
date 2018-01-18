139fe7f697a8b48467d05b9cf2131bbbcbbd2ae7
abdkamal <abdkamal@capgemini.com>
2018-01-18 11:02:24 +0800

    made it abstract

--- Demo-Inheritance/src/com/cg/beans/Feline.java
+++ Demo-Inheritance/src/com/cg/beans/Feline.java
@@ -1,20 +1,20 @@
 package com.cg.beans;
 
-public class Feline extends Animal
+abstract public class Feline extends Animal
 {
     public Feline()
     {
 	// TODO Auto-generated constructor stub
 	System.out.println("instantiating Feline type....");
     }
-    
+
     @Override
     public void roam()
     {
-        // TODO Auto-generated method stub
-        felineRoam();
+	// TODO Auto-generated method stub
+	felineRoam();
     }
-    
+
     private void felineRoam()
     {
 	System.out.println("Roam independently...");
