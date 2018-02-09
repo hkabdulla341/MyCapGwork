e843e081b65f76bbd62318646fe36b6aaf5a734b
abdkamal <abdkamal@capgemini.com>
2018-02-09 16:53:45 +0800

    Different configs for different dbs

--- TestDBConnectivity/src/Entry2.java
+++ TestDBConnectivity/src/Entry2.java
@@ -3,41 +3,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.*;
 
-public class Entry
+public class Entry2
 {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException
     {
 
 	// TODO LOAD JDBC DRIVER into MAIN Memory
+	String mySQLClassName = "com.mysql.jdbc.Driver";
 
-	// String className =
-	// "com.mysql.jdbc.Driver";oracle.jdbc.driver.OracleDriver
-	String className = "oracle.jdbc.driver.OracleDriver";
-
-	Class.forName(className);
+	Class.forName(mySQLClassName);
 
 	// TODO GET DB connection using JDBC URL
 
-	String jdbcURL = "jdbc:oracle:thin:@localhost:1521:xe";
-
-	Connection dbConnection = DriverManager.getConnection(jdbcURL, "hr", "hr");
+	String mysqljdbcURL = "jdbc:mysql://localhost:3306/world";
+	Connection dbConnection = DriverManager.getConnection(mysqljdbcURL, "root", "root");
 	System.out.println("Connection obtained? " + (dbConnection != null));
-
-	System.out.println(dbConnection);
 
 	// TODO CREATE STATEMENT TO EXECUTE A QUERY (?????)
 
 	Statement stmt = dbConnection.createStatement();
 
 	// TODO Getting the result
-	ResultSet rs = stmt.executeQuery("select * from employees");
+	ResultSet rs = stmt.executeQuery("select * from city where id = 88");
 
-	while (rs.next()) 
+	while (rs.next())
 	{
 	    System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4)
-		    + " " + rs.getString(5) + " " + rs.getDate(6) + "  " + rs.getString(7) + " " + rs.getDouble(8) + " "
-		    + rs.getDouble(9) + " " + rs.getInt(10) + " " + rs.getInt(11) + System.lineSeparator());
+		    + " " + rs.getInt(5) + System.lineSeparator());
 	}
 
     }
